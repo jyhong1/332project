9bc98bf7e4deaf0b6c4a61d44862f00d3317e24a
jyhong1 <wmbhoph1@gmail.com>
2022-11-16 21:15:26 +0900

    modify generate_input.sh

--- generate_input.sh
+++ generate_input.sh
@@ -10,9 +10,9 @@ mkdir ./dataset
 
 for i in $(seq 1 $NUM_FILE)
 do
-	mkdir ./dataset/input
+	#mkdir ./dataset/input
 	let idx=($i-1)*$Index
-	./Downloads/gensort-linux-1.5/64/gensort -a -b$idx $Index ./dataset/input/input$i
+	./Downloads/gensort-linux-1.5/64/gensort -a -b$idx $Index ./dataset/input$i
 done
 
 echo "Data generation complete"

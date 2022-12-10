17a766b5791689f583fe85346c522623737d97e7
jyhong1 <wmbhoph1@gmail.com>
2022-12-11 02:54:23 +0900

    Modify sampleMaker, delete temp dirs since start

--- worker/src/main/scala/phases/sampleMaker.scala
+++ worker/src/main/scala/phases/sampleMaker.scala
@@ -11,23 +11,37 @@ import common.Utils
 object sampleMaker {
   def sampling(inputFilePath: String): Seq[String] = {
     val bufferedSource = Utils.getFile(inputFilePath)
-    val sampleRatio = 20
+    val sampleRatio = 20.0
     var numItems = bufferedSource.length
     var numSamples:Int = 0
 
-    if((sampleRatio / 100).toFloat * numItems < 1){
+    println("bufferlength", bufferedSource.length)
+    println("inputFilePath",inputFilePath)
+
+    if((sampleRatio / 100.0).toFloat * numItems.toFloat < 1.0){
       numSamples = numSamples  + 1
     } else {
       numSamples = ((sampleRatio / 100).toFloat * numItems).toInt 
     }
+    //println("numSamples",numSamples.toString())
+
     val (samples,remains) = bufferedSource.splitAt(numSamples)
     val result = samples.map(_.slice(0, 10)).toSeq
+    //println("result\n")
+    result.foreach(println)
+
     result
   }
 
   def makeSamples(inputPaths: List[String]): Seq[String] = {
     val samples =
       inputPaths.flatMap(path => sampling(path)).toSeq 
+    
+    println("inputPaths\n")
+    inputPaths.foreach(println)
+    println("samples\n")
+    samples.foreach(println)
+
     samples
   }
 }
